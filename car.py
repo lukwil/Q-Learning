ed87fc15777ea40739f536a71a0cd797fd0159dc
Lukas Willburger <lukas.willburger@stud.hs-kempten.de>
2019-06-14 10:54:20 +0200

    Working solution with decaying

--- car.py
+++ car.py
@@ -1,60 +1,77 @@
 import gym
 import numpy as np
 
-
 # The mountain car environment has got two observation values: x-axis pos. [-1.2, 0.6] and velocity [-0.07, 0.07]
-# Possible actions are: push left (0), no push (1), push right(2)
+# (via env.observation_space.high, env.observation_space.low)
+# Possible actions are: push left (0), no push (1), push right(2) (via env.action_space.n & documentation)
+# Goal: reach the flag
 env = gym.make("MountainCar-v0")
 
 
-LEARNING_RATE = 0.1
-DISCOUNTING_FACTOR = 0.95
-EPISODES = 25000
+LEARNING_RATE = 0.5
+DISCOUNTING_FACTOR = 0.9
 
-SHOWTIME = 5000
+epsilon = 1
+epsilon_decay = 0.05
 
-# discretization due to granular observation values (20 x 20 shape)
-DISCRETE_OBSERVATION_SPACE = [20] * 2
+EPISODES = 10000
+SHOWTIME = 1000
+
+# discretization due to granular observation values (10 x 10 shape)
+DISCRETE_OBSERVATION_SPACE = [10, 10]
 discrete_step_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OBSERVATION_SPACE
 
 print(discrete_step_size)
 
-#test
+
 def convert_to_discrete_value(state):
-    discrete_state = (state - env.observation_space.low) / discrete_step_size
-    return tuple(discrete_state.astype(np.int))
+    state = (state - env.observation_space.low) / discrete_step_size
+    return tuple(state.astype(np.int))
 
 
-# Forming a 20 x 20 x 3 matrix
+# Forming a 10 x 10 x 3 matrix
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OBSERVATION_SPACE + [env.action_space.n]))
 
 
 for episode in range(EPISODES):
-    if episode % SHOWTIME == 0:
-        render = True
-        print(episode)
-    else:
-        render = False
-
     discrete_state = convert_to_discrete_value(env.reset())
     done = False
 
+    if episode % SHOWTIME == 0:
+        render_current_episode = True
+        print("rendered in episode: ", episode)
+    else:
+        render_current_episode = False
+
     while not done:
-        action = np.argmax(q_table[discrete_state])
+        if np.random.random() > epsilon:
+            # retreive q-table action
+            action = np.argmax(q_table[discrete_state])
+        else:
+            # randomize action
+            action = env.action_space.sample()
+
         new_state, reward, done, _ = env.step(action)
         new_discrete_state = convert_to_discrete_value(new_state)
 
-        if render:
+        if render_current_episode:
             env.render()
+
         if not done:
             max_future_q = np.max(q_table[new_discrete_state])
             current_q = q_table[discrete_state + (action, )]
+            # q-learing formula
             new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNTING_FACTOR * max_future_q)
             q_table[discrete_state + (action,)] = new_q
+
         elif new_state[0] >= env.goal_position:
             q_table[discrete_state + (action,)] = 0
-            print("geschafft: ", episode)
+            print("nailed it: ", episode)
+
+        discrete_state = new_discrete_state
+
+        # perform decaying
+        if EPISODES/2 >= episode:
+            epsilon -= epsilon_decay
+
 env.close()
-
-
-
